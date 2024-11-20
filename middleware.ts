d2d066c62da6afe342500d8a505f5b6925ac0fe0
Noirakumaaa <144528285+Noirakumaaa@users.noreply.github.com>
2024-11-20 11:55:27 +0800

    remove some comments

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,11 @@ import { jwtVerify } from 'jose';
 
 export async function middleware(request: NextRequest) {
   const corsHeaders = {
-    'Access-Control-Allow-Origin': '*', // Allow all origins or a specific one like 'http://localhost:3000'
-    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS', // Allowed HTTP methods
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization', // Allowed headers
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
   };
 
-  // Handle OPTIONS preflight request (for CORS)
   if (request.method === 'OPTIONS') {
     return new NextResponse(null, {
       status: 200,
@@ -16,17 +15,14 @@ export async function middleware(request: NextRequest) {
     });
   }
 
-  // Public routes (like login, sign-up) don't require token verification
   if (request.url.includes('/api/login')) {
     const response = NextResponse.next();
-    // Add CORS headers to the response for login route
     response.headers.set('Access-Control-Allow-Origin', corsHeaders['Access-Control-Allow-Origin']);
     response.headers.set('Access-Control-Allow-Methods', corsHeaders['Access-Control-Allow-Methods']);
     response.headers.set('Access-Control-Allow-Headers', corsHeaders['Access-Control-Allow-Headers']);
     return response;
   }
 
-  // Token verification logic for protected routes
   const token = request.cookies.get('token');
   const secretKey = new TextEncoder().encode(process.env.SECRET_KEY);
 
@@ -35,18 +31,12 @@ export async function middleware(request: NextRequest) {
   }
 
   try {
-    // Verify the JWT token
     const { payload } = await jwtVerify(token.value, secretKey);
-    console.log("Verified Payload:", payload);
-
     const response = NextResponse.next();
-
-    // Set the CORS headers for authenticated routes
     response.headers.set('Access-Control-Allow-Origin', corsHeaders['Access-Control-Allow-Origin']);
     response.headers.set('Access-Control-Allow-Methods', corsHeaders['Access-Control-Allow-Methods']);
     response.headers.set('Access-Control-Allow-Headers', corsHeaders['Access-Control-Allow-Headers']);
     response.headers.set('x-user', JSON.stringify(payload));
-
     return response;
   } catch (error) {
     return NextResponse.json({ error: 'Invalid token', details: error }, { status: 401 });
@@ -54,5 +44,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/api/v1/:path*', '/v1/:path*', '/api/login'], // Include /api/login in the matcher
+  matcher: ['/api/v1/:path*', '/v1/:path*', '/api/login'],
 };
